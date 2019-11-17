440b6b3712ef1f6c6774eceb5acb410682bebdf1
siyuan <xinfeiyang-2008@163.com>
2019-11-17 12:57:39 +0800

    update cmake to support more llvm versions

--- cmake/Modules/Findlibclang.cmake
+++ cmake/Modules/Findlibclang.cmake
@@ -1,7 +1,15 @@
 INCLUDE( FindPackageHandleStandardArgs )
 
 IF( "${LIBCLANG_DIR}" STREQUAL "" )
-  execute_process(COMMAND llvm-config --prefix OUTPUT_VARIABLE LIBCLANG_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(llvm_config_names llvm-config)
+  foreach(major RANGE 9 3)
+    list(APPEND llvm_config_names "llvm-config${major}" "llvm-config-${major}")
+    foreach(minor RANGE 9 0)
+      list(APPEND llvm_config_names "llvm-config${major}${minor}" "llvm-config-${major}.${minor}" "llvm-config-mp-${major}.${minor}")
+    endforeach ()
+  endforeach ()
+  find_program(LIBCLANG_LLVM_CONFIG_EXECUTABLE NAMES ${llvm_config_names})
+  execute_process(COMMAND ${LIBCLANG_LLVM_CONFIG_EXECUTABLE} --prefix OUTPUT_VARIABLE LIBCLANG_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 ENDIF()
 
 FIND_PATH( LIBCLANG_INCLUDE_DIR
